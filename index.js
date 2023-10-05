bd97e253c28091784fb66a9030c38fa91b4196d1
Ankit Tehlan <ankitehlan@gmail.com>
2023-10-05 23:39:13 +0530

    Added implementation for register and login

---
+++ index.js
@@ -0,0 +1,103 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import session from "express-session";
+import routes from "./router/friends.js";
+
+const app = express();
+
+let users = [];
+
+const doesExist = (username) => {
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  if (userswithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+app.use(
+  session({ secret: "fingerpint", resave: true, saveUninitialized: true })
+);
+app.use(express.json());
+
+app.use("/friends", function auth(req, res, next) {
+  if (req.session.authorization) {
+    token = req.session.authorization["accessToken"];
+    jwt.verify(token, "access", (err, user) => {
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 60 }
+    );
+
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
+  }
+});
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(`username:${username},password:${password}`);
+
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({ username: username, password: password });
+      return res.status(200).json({ message: "User successfully registered" });
+    } else {
+      return res.status(404).json({ message: "User already exist" });
+    }
+  } else {
+    return res.status(404).json({ message: "Unable to register username" });
+  }
+});
+
+const PORT = 5001;
+
+app.use("/friends", routes);
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
---
+++ router/friends.js
@@ -0,0 +1,54 @@
+import express from "express";
+
+const router = express.Router();
+
+let friends = {
+  "ankitehlan@gamil.com": {
+    firstName: "Ankit",
+    lastName: "Tehlan",
+    DOB: "20-06-1990",
+  },
+  "ankitehlan.work@gamil.com": {
+    firstName: "Ahana",
+    lastName: "Tehlan",
+    DOB: "20-10-2017",
+  },
+  "Swatitehlan22@gamil.com": {
+    firstName: "Swati",
+    lastName: "Tehlan",
+    DOB: "22-08-1995",
+  },
+};
+
+// GET request: Retrieve all friends
+router.get("/", (req, res) => {
+  // Update the code here
+
+  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// GET by specific ID request: Retrieve a single friend with email ID
+router.get("/:email", (req, res) => {
+  // Update the code here
+  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// POST request: Add a new friend
+router.post("/", (req, res) => {
+  // Update the code here
+  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// PUT request: Update the details of a friend with email id
+router.put("/:email", (req, res) => {
+  // Update the code here
+  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// DELETE request: Delete a friend by email id
+router.delete("/:email", (req, res) => {
+  // Update the code here
+  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+export default router;
